785f63298c87e2b3aaccaebd915b36b92b56b5b2
senple <semple.python@gmail.com>
2020-10-15 20:12:55 +0900

    add Vuetify

--- src/components/three-column.vue
+++ src/components/three-column.vue
@@ -1,20 +1,13 @@
 <template>
   <v-app id="inspire">
     <v-app-bar app color="white" flat>
-      <v-avatar
-        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
-        size="32"
-      ></v-avatar>
+      <v-avatar :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'" size="32"></v-avatar>
 
       <v-tabs centered class="ml-n9" color="grey darken-1">
         <v-tab v-for="link in links" :key="link">{{ link }}</v-tab>
       </v-tabs>
 
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey darken-1 shrink"
-        size="32"
-      ></v-avatar>
+      <v-avatar class="hidden-sm-and-down" color="grey darken-1 shrink" size="32"></v-avatar>
     </v-app-bar>
 
     <v-main class="grey lighten-3">
@@ -46,7 +39,7 @@
 <script>
 export default {
   data: () => ({
-    links: ["About", "Skill", "Travel", "Contact"]
+    links: ["Dashboard", "Messages", "Profile", "Updates"]
   })
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import vuetify from './plugins/vuetify';
 
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
@@ -16,5 +17,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
